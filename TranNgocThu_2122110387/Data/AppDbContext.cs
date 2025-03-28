4baf357d51dd98c843046253b9878e4fe59b4659
TranThu03 <thu257783@gmail.com>
2025-03-28 16:30:10 +0700

    first commit

---
+++ TranNgocThu_2122110387/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TranNgocThu_2122110387.Model;
+namespace TranNgocThu_2122110387.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
